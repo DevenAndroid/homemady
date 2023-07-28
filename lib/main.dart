bb70d04098205a366c969cde2cf7a187a2188b98
DevenAndroid <deveneoxys@gmail.com>
2023-07-28 17:54:04 +0530

    28 App Uploading

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -9,8 +9,8 @@ import 'package:homemady/routers/routers.dart';
 Future<void> main() async {
 
 
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
